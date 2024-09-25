21284a2b07e3b96b7dd4fb71f0ce327391a686a7
MrSykenro <kemalguksek@gmail.com>
2024-09-25 22:10:53 +0300

    changed hand_recognition.py
    changed script to be used as a library

--- src/python/hand_recognition.py
+++ src/python/hand_recognition.py
@@ -8,35 +8,22 @@ mp_drawing = mp.solutions.drawing_utils
 cap = cv2.VideoCapture(0)
 
 
-def main():
-    0
-
-
 # Initialize Pose and Hands models
 with mp_pose.Pose(static_image_mode=False, min_detection_confidence=0.4, min_tracking_confidence=0.7) as pose, \
      mp_hands.Hands(static_image_mode=False, max_num_hands=2, min_detection_confidence=0.4, min_tracking_confidence=0.7) as hands:
-
-
-    # Perform pose and hands detection !!! Use only one of those functions at the same time !!!
-    def process_pose_and_hands():
-        global pose_results, hand_results
-        image_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        pose_results = pose.process(image_rgb)
-        hand_results = hands.process(image_rgb)
     # Perform hands detection
-    def process_hands():
-        global hand_results
-        image_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        hand_results = hands.process(image_rgb)
+    def process_hands(frame):  # Takes a RGB image
+        #global hand_results
+        hand_results = hands.process(frame)
+        return hand_results
     # Perform pose detection
-    def process_pose():
-        global pose_results
-        image_rgb = cv2.cvtColor(frame, cv2.COLOR_BGR2RGB)
-        pose_results = pose.process(image_rgb)
-
+    def process_pose(frame):  # Takes a RGB image
+        #global pose_results
+        pose_results = pose.process(frame)
+        return pose_results
 
     # Draw pose landmarks on the frame if detection is successful
-    def draw_pose():
+    def draw_pose(frame, pose_results):
         if pose_results.pose_landmarks:
             mp_drawing.draw_landmarks(
                 frame,
@@ -45,7 +32,7 @@ with mp_pose.Pose(static_image_mode=False, min_detection_confidence=0.4, min_tra
                 mp_drawing.DrawingSpec(color=(255, 0, 0), thickness=2, circle_radius=2),  # Pose landmark circles
                 mp_drawing.DrawingSpec(color=(0, 255, 0), thickness=2, circle_radius=2))   # Pose connection lines
     # Draw hand landmarks on the frame if hands are detected
-    def draw_hands():
+    def draw_hands(frame, hand_results):
         if hand_results.multi_hand_landmarks:
             for hand_landmarks in hand_results.multi_hand_landmarks:
                 mp_drawing.draw_landmarks(
@@ -54,28 +41,35 @@ with mp_pose.Pose(static_image_mode=False, min_detection_confidence=0.4, min_tra
                     mp_hands.HAND_CONNECTIONS,
                     mp_drawing.DrawingSpec(color=(0, 0, 255), thickness=2, circle_radius=2),  # Hand landmark circles
                     mp_drawing.DrawingSpec(color=(255, 255, 0), thickness=2, circle_radius=2))  # Hand connection lines
+                
+    def print_hand_landmarks(hand_results):
+        if hand_results.multi_hand_landmarks:
+            for hand_landmarks in hand_results.multi_hand_world_landmarks:
+                print(hand_landmarks)
 
+    def main():
+        while cap.isOpened():
+            success, image_bgr = cap.read()
+            if not success:
+                print("Ignoring empty camera frame.")
+                continue    
+            
+            image_rgb = cv2.cvtColor(image_bgr, cv2.COLOR_BGR2RGB)
+            pose_result = process_pose(image_bgr)
+            hand_result = process_hands(image_rgb)
+            draw_pose(image_bgr, pose_result)
+            draw_hands(image_bgr, hand_result)
+            print_hand_landmarks(hand_result)
+    
+            # Display the frame with pose and hand landmarks
+            cv2.imshow('MediaPipe Detection Results', image_bgr)
+    
+            # Break loop on 'q' key press
+            if cv2.waitKey(5) & 0xFF == 27:
+                # Release the webcam and close the OpenCV window
+                cap.release()
+                cv2.destroyAllWindows()
 
-    while cap.isOpened():
-        success, frame = cap.read()
-        if not success:
-            print("Ignoring empty camera frame.")
-            continue
-
-        process_pose_and_hands()
-        draw_pose()
-        draw_hands()
-
-        # Display the frame with pose and hand landmarks
-        cv2.imshow('MediaPipe Detection Results', frame)
-
-        # Break loop on 'q' key press
-        if cv2.waitKey(5) & 0xFF == 27:
-            break
-
-# Release the webcam and close the OpenCV window
-cap.release()
-cv2.destroyAllWindows()
 
 if __name__ == "__main__":
     main()
