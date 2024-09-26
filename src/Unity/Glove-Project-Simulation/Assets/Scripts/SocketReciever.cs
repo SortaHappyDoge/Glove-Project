3df966a8a80c5e65b24fb26580886698389b0f95
SortaHappyDoge <ocalakif1@gmail.com>
2024-09-26 13:43:25 +0300

    modified socket reciever in unity

--- src/Unity/Glove-Project-Simulation/Assets/Scripts/SocketReciever.cs
+++ src/Unity/Glove-Project-Simulation/Assets/Scripts/SocketReciever.cs
@@ -4,7 +4,6 @@ using System.Net.Sockets;
 using System.Text;
 using System.Threading;
 using UnityEngine;
-using System;
 using System.Runtime.InteropServices;
 
 [StructLayout(LayoutKind.Sequential, Pack = 1)]
@@ -17,19 +16,41 @@ public struct Recieved
 
 public class SocketReciever : MonoBehaviour
 {
-    UdpClient client = new UdpClient();
-    IPEndPoint ep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8000);
+    System.Threading.Thread SocketThread;
     
-    void Start()
+    public void Start()
     {
-        client.Connect(ep);
-    }
-    private void Update()
-    {
-        var recieved = client.Receive(ref ep);
-        
+        Application.runInBackground = true;
+        StartServer();
+        new Thread(() =>
+        {
+            /*SocketReciever client = new SocketReciever();
+            IPEndPoint ep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8000);
+            client.Connect(ep);
+            */
+            IPEndPoint ep = new IPEndPoint(IPAddress.Any, 8001);
+            Socket newsock = new Socket(AddressFamily.InterNetwork, socketType:SocketType.Dgram, protocolType:ProtocolType.Udp);
+            newsock.Bind(ep);
 
-        float[] split = Array.ConvertAll(recieved.ToString().Split(","), float.Parse);
-        Recieved message; message.pitch = split[0]; message.roll = split[1];
+            while (true)
+            {
+                byte[] data;
+                newsock.Receive(data);
+                print(recieved.ToString());
+
+                float[] split = Array.ConvertAll(recieved.ToString().Split(","), float.Parse);
+                Recieved message; message.pitch = split[0]; message.roll = split[1];
+                transform.rotation = new Quaternion(split[0], 0, split[1], 0);
+            }
+        }).Start();
+        
+    }
+    void StartServer()
+    {
+        SocketThread = new System.Threading.Thread(NetworkCode())
+    }
+    void NetworkCode()
+    {
+       
     }
 }
