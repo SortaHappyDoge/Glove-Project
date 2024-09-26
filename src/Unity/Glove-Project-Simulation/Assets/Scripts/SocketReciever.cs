7c67d42628af340f0ea328555673b48cf3861890
SortaHappyDoge <ocalakif1@gmail.com>
2024-09-26 09:54:42 +0300

    modified unity socket reciever

--- src/Unity/Glove-Project-Simulation/Assets/Scripts/SocketReciever.cs
+++ src/Unity/Glove-Project-Simulation/Assets/Scripts/SocketReciever.cs
@@ -8,73 +8,28 @@ using System;
 using System.Runtime.InteropServices;
 
 [StructLayout(LayoutKind.Sequential, Pack = 1)]
-public struct MyStruct
+public struct Recieved
 {
-    public float Field1;
-    public float Field2;
-    public int Field3;
+    public float pitch;
+    public float roll;
+    public int id;
 }
 
 public class SocketReciever : MonoBehaviour
 {
-    private UdpClient udpClient;
-    private Thread receiveThread;
-    private bool isReceiving = true;
-    public int port = 8000;
-
+    UdpClient client = new UdpClient();
+    IPEndPoint ep = new IPEndPoint(IPAddress.Parse("127.0.0.1"), 8000);
+    
     void Start()
     {
-        udpClient = new UdpClient(port);
-        receiveThread = new Thread(new ThreadStart(ReceiveData));
-        receiveThread.IsBackground = true;
-        receiveThread.Start();
+        client.Connect(ep);
     }
-
-    private void ReceiveData()
+    private void Update()
     {
-        while (isReceiving)
-        {
-            try
-            {
-                IPEndPoint remoteEndPoint = new IPEndPoint(IPAddress.Any, port);
-                byte[] data = udpClient.Receive(ref remoteEndPoint);
-                string message = Encoding.UTF8.GetString(data);
-                Debug.Log("Received: " + message);
-                
-                // You can now process the message, such as parsing it into a format you need
-            }
-            catch (Exception e)
-            {
-                Debug.LogError("Error receiving data: " + e.Message);
-            }
-        }
-    }
+        var recieved = client.Receive(ref ep);
+        
 
-    public static MyStruct ByteArrayToStruct(byte[] byteArray)
-    {
-        // Pin the array to a fixed memory address.
-        GCHandle handle = GCHandle.Alloc(byteArray, GCHandleType.Pinned);
-
-        try
-        {
-            // Get the pointer to the data in the byte array.
-            IntPtr ptr = handle.AddrOfPinnedObject();
-
-            // Convert the byte array (pointer) into the struct.
-            return Marshal.PtrToStructure<MyStruct>(ptr);
-        }
-        finally
-        {
-            // Release the pinned memory handle.
-            handle.Free();
-        }
-    }
-
-
-    void OnApplicationQuit()
-    {
-        isReceiving = false;
-        if (receiveThread != null) receiveThread.Abort();
-        udpClient.Close();
+        float[] split = Array.ConvertAll(recieved.ToString().Split(","), float.Parse);
+        Recieved message; message.pitch = split[0]; message.roll = split[1];
     }
 }
