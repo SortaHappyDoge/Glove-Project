48f166e3bb8722b219e702efabdde0708bb7c892
SortaHappyDoge <ocalakif1@gmail.com>
2024-12-08 18:15:09 +0300

    created joint monitoring.cs, can now see the angles between every joint in the hands

--- src/Unity/Glove-Project-Simulation/Assets/Scripts/AngleCalculation.cs
+++ src/Unity/Glove-Project-Simulation/Assets/Scripts/AngleCalculation.cs
@@ -7,20 +7,23 @@ public class AngleCalculation : MonoBehaviour
     [SerializeField] GameObject point_1;
     [SerializeField] GameObject point_2;
 
+    public string angleName;
+
     public Vector3 angleDirection = Vector3.one;
     GameObject middle_point;
 
     void Start(){
-        middle_point = point_0;
+        middle_point = point_1;
     }
-    void Update(){
-        Vector3 point_1_to_mid = point_1.transform.position - middle_point.transform.position;
+
+    public double GetAngle(){
+        Vector3 point_0_to_mid = point_0.transform.position - middle_point.transform.position;
         Vector3 point_2_to_mid = point_2.transform.position - middle_point.transform.position;
 
-        float angle = Vector3.SignedAngle(point_1_to_mid, point_2_to_mid, angleDirection);
+        float angle = Vector3.SignedAngle(point_0_to_mid, point_2_to_mid, angleDirection);
 
         if(angle >= 0) angle = angle;
         else angle = 360 + angle;
-        Debug.Log(angle);
+        return angle;
     }
 }
---
+++ src/Unity/Glove-Project-Simulation/Assets/Scripts/JointMonitoring.cs
@@ -0,0 +1,35 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class JointMonitoring : MonoBehaviour
+{
+    public List<AngleCalculation> leftHandJoints = new List<AngleCalculation>();
+    public List<AngleCalculation> rightHandJoints = new List<AngleCalculation>();
+
+
+    void Update(){
+        FetchAngles(0);
+    }
+
+    void FetchAngles(int options){
+        if(options == 0){
+            foreach(AngleCalculation joint in leftHandJoints){
+                Debug.Log(joint.angleName+" "+joint.GetAngle());
+            }
+            foreach(AngleCalculation joint in rightHandJoints){
+                Debug.Log(joint.angleName+" "+joint.GetAngle());
+            }
+        }
+        else if(options == 1){
+            foreach(AngleCalculation joint in leftHandJoints){
+                Debug.Log(joint.angleName+" "+joint.GetAngle());
+            }
+        }
+        else{
+            foreach(AngleCalculation joint in rightHandJoints){
+                Debug.Log(joint.angleName+" "+joint.GetAngle());
+            }
+        }
+    }
+}
